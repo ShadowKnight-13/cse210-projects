c0e1fa4eac282a6612c719ed91de8be916cc3146
ShadowKnight-13 <padrago@yahoo.com>
2025-09-18 11:15:50 -0600

    Update Program.cs

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,64 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($"My name is {lastName}, {firstName} {lastName}.");
+
+
+
+
+
+
+
+
+
+
+        // string firstName = "BOb";
+
+        //Console.WriteLine("Hello " + firstName);
+        // Console.WriteLine($"Hello {firstName}");
+        //Console.Write($"Hello Bob");
+        //Console.Write("This is another line\n");
+
+        //Console.Write("What is your favorite color? ");
+        //string favoriteColor = Console.ReadLine();
+
+        //Console.WriteLine($"{firstName}, your favorite color is {favoriteColor}.");
+
+        //Console.Write("How old are you? ");
+        //int age = int.Parse(Console.ReadLine());
+        //Console.WriteLine($"{firstName}, you are {age} years old.");
+
+        //int a = 13;
+
+        //int b = 7;
+
+        //int c = 1 + b;
+
+        //Console.WriteLine($"C is {c}");
+
+        //Console.WriteLine($"a/b is {a / b}");
+        //Console.WriteLine($"{(double)a / b}");
+
+        //double x = 1.5;
+
+        //float y = 1.5f;
+
+        //bool isDone = false;
+
+        //if (isDone)
+        // {
+        //    Console.WriteLine("All done.");
+        // }
+        //else
+        // {
+        //     Console.WriteLine("Not done yet");
+        //}
+        //
+
     }
 }
