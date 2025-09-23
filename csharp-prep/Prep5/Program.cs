c021aabcda70c4a02f0698d48bfcd98acd61466d
ShadowKnight-13 <padrago@yahoo.com>
2025-09-23 14:37:55 -0600

    started working on assignment 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -6,4 +6,25 @@ class Program
     {
         Console.WriteLine("Hello Prep5 World!");
     }
+
+    static void DisplayWelcom() { Console.WriteLine("Welcome to the Program"); }
+
+    static string PromptUserName()
+    {
+        Console.WriteLine("Please type your name: ");
+        string input = Console.ReadLine();
+
+        return input;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.WriteLine("What is your favorite number? ");
+        string input = Console.ReadLine();
+        int number = int.Parse(input);
+
+        return number;
+    }
+
+    
 }
