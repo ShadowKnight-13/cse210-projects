3572cd4e3027ee0f8f182b6ed69b6fb643c89116
ShadowKnight-13 <padrago@yahoo.com>
2025-10-16 14:32:39 -0600

    started on prove 03

---
+++ prove/Develop03/references.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Reference
+{
+    private string book;
+    private int chapter;
+    private int startVerse;
+    private int endVerse;
+
+    public void GetSingleVerse()
+    {
+        Console.WriteLine("Please enter the scripture reference following the example. (Example: John 4:3)");
+        string input = Console.ReadLine();
+
+           
+    }
+
+}
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,70 @@
 using System;
+using System.Diagnostics.Metrics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        List<int> numbers = [0, 1, 2, 3, 4];
+        List<string> words = ["Hello", "World", "I", "am", "Parker"];
+        List<string> blankList = [];
+
+        foreach (string word in words)
+        {
+            Console.Write(word + " ");
+        }
+        Console.Write("\n");
+
+        string input = "";
+        int counter1 = 0;
+        while (counter1 < words.Count() & input != "quit")
+        {
+
+            blankList.Clear();
+            foreach (string word in words)
+            {
+                blankList.Add("Blank");
+            }
+
+            Random randomGenerator = new Random();
+            int ranNumber = randomGenerator.Next(1, numbers.Count());
+
+            numbers.RemoveAt(ranNumber - 1);
+            foreach (int number in numbers)
+            {
+                blankList[number] = "Not Blank";
+            }
+
+            int counter = 0;
+            while (counter < words.Count())
+            {
+                if (blankList[counter] == "Blank")
+                {
+                    foreach (char letter in words[counter])
+                    {
+                        Console.Write("_");
+                    }
+                    Console.Write(" ");
+                }
+                else
+                {
+                    Console.Write(words[counter] + " ");
+                }
+                counter++;
+            }
+            Console.Write("\n");
+            counter1++;
+
+            input = Console.ReadLine();
+            try
+            {
+                Console.Clear();
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("Console.Clear() failed.");
+            }
+
+        }
     }
 }
